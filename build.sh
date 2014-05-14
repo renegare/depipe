3971fb8c900fbc97880c3a8b153b55f52ec686a6
Mudi Ugbowanko <mudi.ugbowanko@mendeley.com>
2014-05-14 17:17:30 +0100

    add build phar script

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+cd "$(dirname $0)"
+
+echo "----------------------------------------------------------------------------------------------------"
+echo "Checking for build deps"
+echo "----------------------------------------------------------------------------------------------------"
+if [[ "$(box --version)" == *"Box"* ]]; then
+    echo "box executable found"
+else
+    echo "ERROR: box executable not found (see https://github.com/herrera-io/php-box)"
+    exit 127
+fi
+
+echo "$(composer --version)"
+
+if [[ "$(composer --version)" == *"Composer"* ]]; then
+    echo "box executable found"
+else
+    echo "ERROR: composer executable not found (see https://getcomposer.org/download/)"
+    exit 127
+fi
+echo ""
+
+echo "----------------------------------------------------------------------------------------------------"
+echo "Updating Composer without dev deps"
+echo "----------------------------------------------------------------------------------------------------"
+composer update --no-dev --prefer-dist
+echo ""
+
+echo "----------------------------------------------------------------------------------------------------"
+echo "Building PHAR with box"
+echo "----------------------------------------------------------------------------------------------------"
+box build
+echo ""
+
+echo "----------------------------------------------------------------------------------------------------"
+echo "Restoring Composer dev deps"
+echo "----------------------------------------------------------------------------------------------------"
+composer update --prefer-dist
+echo ""
+
+echo "----------------------------------------------------------------------------------------------------"
+echo "Build complete"
+echo "----------------------------------------------------------------------------------------------------"
